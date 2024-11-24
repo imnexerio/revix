87040b8549e2bddef3d8509c934beabf37020972
santosh <santosh52628@gmail.com>
2024-11-24 19:02:10 +0530

    fixed date scheduling issue

--- lib/Utils/date_utils.dart
+++ lib/Utils/date_utils.dart
@@ -1,25 +1,23 @@
 class DateNextRevision {
   static DateTime calculateNextRevisionDate(DateTime scheduledDate, String frequency, int noRevision) {
-  switch (frequency) {
-    case 'Daily':
-      return scheduledDate.add(Duration(days: 1));
-    case '2 Day':
-      return scheduledDate.add(Duration(days: 2));
-    case '3 Day':
-      return scheduledDate.add(Duration(days: 3));
-    case 'Weekly':
-      return scheduledDate.add(Duration(days: 7));
-    case 'Default':
-    default:
-      List<int> intervals = [1, 3, 7, 15, 30];
-      int additionalDays = 0;
-      for (int i = 0; i <= noRevision; i++) {
-        additionalDays += (i < intervals.length) ? intervals[i] : 30;
-      }
-      return scheduledDate.add(Duration(days: additionalDays));
+    switch (frequency) {
+      case 'Daily':
+        return scheduledDate.add(Duration(days: 1));
+      case '2 Day':
+        return scheduledDate.add(Duration(days: 2));
+      case '3 Day':
+        return scheduledDate.add(Duration(days: 3));
+      case 'Weekly':
+        return scheduledDate.add(Duration(days: 7));
+      case 'Default':
+      default:
+        List<int> intervals = [1, 4, 7, 15, 30];
+        int additionalDays = (noRevision < intervals.length)
+            ? intervals[noRevision]
+            : 30;  // After using all intervals, use 30 days
+        return scheduledDate.add(Duration(days: additionalDays));
+    }
   }
-}
-
 
   static DateTime calculateFirstScheduledDate(String frequency) {
     DateTime today = DateTime.now();
@@ -37,5 +35,4 @@ class DateNextRevision {
         return today.add(Duration(days: 1));
     }
   }
-
 }
