954d9f4ffc4c19aa16443e84993fe9c193eb109a
imnexerio <imnexerio@gmail.com>
2025-02-22 14:25:26 +0530

    version update

--- lib/LoginSignupPage/SignupPage.dart
+++ lib/LoginSignupPage/SignupPage.dart
@@ -124,12 +124,12 @@ class _SignupPageState extends State<SignupPage>
             "Others"
           ],
           'custom_frequencies': {
-            '2 Day': [1,2],
-            '3 Day': [1,3],
+            '2 Day': [2],
+            '3 Day': [3],
             'Daily': [1],
             'Default': [1, 4, 7, 15, 30, 60],
             'Priority': [1, 3, 4, 5, 7, 15, 25, 30],
-            'Weekly': [1,7],
+            'Weekly': [7],
           },
         });
 
