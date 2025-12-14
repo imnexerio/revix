d8ff93c4cbef110b3ad564b95ca5a84a574a1807
imnexerio <imnexerio@gmail.com>
2025-12-14 14:58:03 +0530

    adding ico

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,7 @@
+// Revix - A powerful task scheduling and productivity application
+// Copyright (C) 2024-2025 imnexerio
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 fn main() {
     tauri_build::build()
 }
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,3 +1,13 @@
+// Revix - A powerful task scheduling and productivity application
+// Copyright (C) 2024-2025 imnexerio
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Repository: https://github.com/imnexerio/revix
+
 // Prevents additional console window on Windows in release
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
